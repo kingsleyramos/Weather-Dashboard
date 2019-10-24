2f0af2017129c33bc514fb898f1709345565bb16
Kingsley Ramos <Kingsleyramos@gmail.com>
2019-10-23 20:50:18 -0700

    Added Current Weather and added default values in index.html

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 $(document).ready(function() {
 
-
+var cityToSearch
 
 // Will Add search to array and local storage
 $("#searchBtn").click(function(){
     console.log("on click")
-    var value = $("#citySearch").val();
-    SearchHistory.unshift(value)
-    console.log("Value" + value)
+    cityToSearch = $("#citySearch").val();
+    SearchHistory.unshift(cityToSearch)
+    console.log("Value" + cityToSearch)
     renderSearchHistory();
     console.log(SearchHistory)
 
@@ -16,6 +16,8 @@ $("#searchBtn").click(function(){
     while (SearchHistory.length > 7){
         SearchHistory.splice(-1,1);
     }
+
+    getWeather();
 });
 
 
@@ -23,6 +25,10 @@ $("#searchBtn").click(function(){
 function renderSearchHistory(){
 
     $("#searchHistory").empty();
+
+    while (SearchHistory.length > 7){
+        SearchHistory.splice(-1,1);
+    }
 
     for(i=0; i<SearchHistory.length; i++){
         cityListItem = $("<li>");
@@ -33,6 +39,44 @@ function renderSearchHistory(){
     }
 }
 
+ function getWeather(){
+
+     
+    var currentWeatherURL = "http://api.openweathermap.org/data/2.5/weather?q=" + cityToSearch + "&units=imperial&appid=f00d24224c5175367e9256f06efb0e78";
+
+    $.ajax({
+    url: currentWeatherURL,
+    method: "GET"
+    }).then(function(response) {
+
+        $("#city").text(response.name);
+
+        var currentWeatherImg = "http://openweathermap.org/img/wn/" + response.weather[0].icon + "@2x.png";
+        // $.ajax({
+        //     url: currentWeatherImg,
+        //     method: "GET"
+        //     }).then(function(UVresponse) {
+        //         $("#currentWeatheriCon").attr("src", "http://openweathermap.org/img/wn/" + response.weather.icon + "@2x.png";);
+        //     });
+
+        
+        $("#currentWeatherIcon").attr("src", currentWeatherImg);
+        console.log(currentWeatherImg)
+        var dateString = moment.unix(response.dt).format("MM/DD/YYYY");
+        $("#currentDateAndTime").text(dateString);
+        $("#currentTempurature").html("Tempurature: " + response.main.temp + " &degF");
+        $("#currentHumidity").text("Humidity: " + response.main.humidity + "%");
+        $("#currentWindSpeed").text("Wind Speed: " + response.wind.speed);
+        
+        var currentUVIndexURL = "http://api.openweathermap.org/data/2.5/uvi?lat=" + response.coord.lat + "5&lon=" + response.coord.lon + "&appid=f00d24224c5175367e9256f06efb0e78"
+        $.ajax({
+            url: currentUVIndexURL,
+            method: "GET"
+            }).then(function(UVresponse) {
+                $("#currentUVIndex").text("UV Index: " + UVresponse.value);
+            });
+    });
+};
 
 
 
@@ -42,6 +86,11 @@ function renderSearchHistory(){
     // will pull from local storage for any history
     var SearchHistory = JSON.parse(localStorage.getItem("SearchHistory"));
     var cityListItem
+    cityToSearch = SearchHistory[0]
+    console.log("searchHistory: " + SearchHistory)
+    console.log("cityToSearch:" + cityToSearch)
+
+    getWeather();
 
     // will render history for the first time at page load
     renderSearchHistory();
@@ -52,6 +101,7 @@ function renderSearchHistory(){
         SearchHistory = [];
         localStorage.setItem("SearchHistory", JSON.stringify(SearchHistory));
     }
+
 
 // ========== RUNNING CODE END ==========
 
